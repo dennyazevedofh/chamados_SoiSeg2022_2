e9e3f077309ca5b35507a4afa2900ed69a6a7087
DennyAzevedo <denny_af@yahoo.com.br>
2022-11-19 15:54:45 -0300

    iniciando o projeto - estabelecendo as rotas

---
+++ src/contexts/auth.js
@@ -0,0 +1,4 @@
+import { useState, createContext, useEffect } from 'react';
+import firebase from '../services/firebaseConnection';
+
+export const AuthContext = createContext({});
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,51 @@
+import { useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/auth';
+import logo from '../../assets/logo.png';
 
 function SignUp() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { signUp } = useContext(AuthContext);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (nome !== '' && email !== '' && password !== '') {
+      signUp(email, password, nome);
+    }
+  }
+
   return (
-    <div>
-      <h1>Cadastro Usuário</h1>
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt="Sistema Logo" />
+        </div>
+        <form onSubmit={handleSubmit}>
+          <h1>Entrar</h1>
+          <input
+            type="text"
+            placeholder="Seu nome"
+            value={nome}
+            onChange={ (e) => setNome(e.target.value) }
+          />
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={ (e) => setEmail(e.target.value) }
+          />
+          <input
+            type="password"
+            placeholder="******"
+            value={password}
+            onChange={ (e) => setPassword(e.target.value) }
+          />
+          <button type="submit">Cadastrar</button>
+        </form>
+        <Link to="/">Já tem uma conta? Entre</Link>
+      </div>
     </div>
   );
 }
