788a5e94be3afcd13b5def146b89c9588aa87241
dennyazevedofh <denny.filho@outlook.com>
2022-12-12 20:42:13 -0300

    criando pagina novo chamado

---
+++ src/pages/New/index.js
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import Header from "../../components/Header";
+import Title from "../../components/Title";
+import "./new.css";
+import { FiPlusCircle } from "react-icons/fi";
+
+export default function New() {
+	const [assunto, setAssunto] = useState("Suporte");
+	const [status, setStatus] = useState("Aberto");
+	const [complemento, setComplemento] = useState("");
+
+	function handleRegister(e) {
+		e.preventDefault();
+		alert("Teste");
+	}
+
+	function handleChangeSelect(e) {
+		setAssunto(e.target.value);
+	}
+
+	function handleOptionChange(e) {
+		setStatus(e.target.value);
+	}
+
+	return (
+		<div>
+			<Header />
+			<div className="content">
+				<Title name="Novo Chamado">
+					<FiPlusCircle size={25} />
+				</Title>
+				<div className="container">
+					<form className="form-profile" onSubmit={handleRegister}>
+						<label>Cliente</label>
+						<select>
+							<option key={1} value={1}>
+								Sujeito Programador
+							</option>
+						</select>
+						<label>Assunto</label>
+						<select>
+							<option value="Suporte">Suporte</option>
+							<option value="Visita Técnica">Visita Técnica</option>
+							<option value="Financeiro">Financeiro</option>
+						</select>
+						<label>Status</label>
+						<div className="status">
+							<input
+								type="radio"
+								name="radio"
+								value="Aberto"
+								onChange={handleOptionChange}
+								checked={status === "Aberto"}
+							/>
+							<span>Em Aberto</span>
+							<input
+								type="radio"
+								name="radio"
+								value="Progresso"
+								onChange={handleOptionChange}
+								checked={status === "Progresso"}
+							/>
+							<span>Progresso</span>
+							<input
+								type="radio"
+								name="radio"
+								value="Atendido"
+								onChange={handleOptionChange}
+								checked={status === "Atendido"}
+							/>
+							<span>Atendido</span>
+						</div>
+						<label>Complemento</label>
+						<textarea
+							type="text"
+							placeholder="Descreva seu problema (opcional)."
+							value={complemento}
+							onChange={(e) => setComplemento(e.target.value)}
+						/>
+						<button type="submit">Registrar</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+}
