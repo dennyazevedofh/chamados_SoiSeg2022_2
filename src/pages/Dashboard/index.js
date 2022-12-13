8c642c8a5ddc5c3ec10c9d707e2e778da9918295
dennyazevedofh <denny.filho@outlook.com>
2022-12-12 21:49:09 -0300

    Ajustando o dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,12 +1,68 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "../../components/Header";
 import Title from "../../components/Title";
 import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import { format } from "date-fins";
+import firebase from "../../services/firebaseConnection";
 import "./dashboard.css";
+
+const listRef = firebase
+	.firestore()
+	.collection("chamados")
+	.orderBy("created", "desc");
 
 export default function Dashboard() {
 	const [chamados, setChamados] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [loadingMore, setLoadingMore] = useState(false);
+	const [isEmpty, setIsEmpty] = useState(false);
+	const [lasDocs, setLastDocs] = useState();
+
+	useEffect(() => {
+		loadChamados();
+		return () => {};
+	}, []);
+
+	async function loadChamados() {
+		await listRef
+			.limit(5)
+			.get()
+			.then((snapshot) => {
+				updateState(snapshot);
+			})
+			.catch((error) => {
+				console.log("Deu algum erro: ", error);
+				setLoadingMore(false);
+			});
+		setLoading(false);
+	}
+
+	async function updateState(snapshot) {
+		const isCollectionEmpty = snapshot.size === 0;
+		if (!isCollectionEmpty) {
+			let lista = [];
+			snapshot.forEach((doc) => {
+				lista.push({
+					id: doc.id,
+					assunto: doc.data().assunto,
+					cliente: doc.data().cliente,
+					clienteId: doc.data().clienteId,
+					created: doc.data().created,
+					creratedFormated: format(doc.data().created.toDate(), "dd/MM/yyy"),
+					status: doc.data().status,
+					complemento: doc.data().complemento,
+				});
+			});
+			//Pegando o ultimo documento buscado
+			const lastDoc = snapshot.docs[snapshot.docs.lenght - 1];
+			setChamados((chamados) => [...chamados, ...lista]);
+			setLastDocs(lasDocs);
+		} else {
+			setIsEmpty(true);
+		}
+		setLoadingMore(false);
+	}
 
 	return (
 		<div>
