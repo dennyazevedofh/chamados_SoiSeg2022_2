93ff398e28d335dc247924036770ca2fd7ef32cd
dennyazevedofh <denny.filho@outlook.com>
2022-12-12 21:23:37 -0300

    Ajustando a gravação

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,17 +1,76 @@
-import { useState } from "react";
+import { useState, useEffect, useContext } from "react";
+import firebase from "../../services/firebaseConnection";
 import Header from "../../components/Header";
 import Title from "../../components/Title";
+import { AuthContext } from "../../contexts/auth";
+import { toast } from "react-toastify";
 import "./new.css";
 import { FiPlusCircle } from "react-icons/fi";
 
 export default function New() {
+	const [loadCustomers, setLoadCustomers] = useState(true);
+	const [customers, setCustomers] = useState([]);
+	const [customerSelected, setCustomerSelected] = useState(0);
 	const [assunto, setAssunto] = useState("Suporte");
 	const [status, setStatus] = useState("Aberto");
 	const [complemento, setComplemento] = useState("");
+	const { user } = useContext(AuthContext);
 
-	function handleRegister(e) {
+	useEffect(() => {
+		async function loadCustomers() {
+			await firebase
+				.firestore()
+				.collection("customers")
+				.get()
+				.then((snapshot) => {
+					let lista = [];
+					snapshot.forEach((doc) => {
+						lista.push({
+							id: doc.id,
+							nomeFantasia: doc.data().nomeFantasia,
+						});
+					});
+					if (lista.length === 0) {
+						console.log("Nenhuma Empresa Encontrada");
+						setCustomers([{ id: 1, nomeFantasia: "Freela" }]);
+						setLoadCustomers(false);
+						return;
+					}
+					setCustomers(lista);
+					setLoadCustomers(false);
+				})
+				.catch((error) => {
+					console.log("Deu algum erro! ", error);
+					setLoadCustomers(false);
+					setCustomers([{ id: 1, nomeFantasia: "Freela" }]);
+				});
+		}
+		loadCustomers();
+	}, []);
+
+	async function handleRegister(e) {
 		e.preventDefault();
-		alert("Teste");
+		await firebase
+			.firestore()
+			.collection("chamados")
+			.add({
+				create: new Date(),
+				cliente: customers[customerSelected].nomeFantasia,
+				clienteId: customers[customerSelected].id,
+				assunto: assunto,
+				status: status,
+				complemento: complemento,
+				userId: user.uid,
+			})
+			.then(() => {
+				toast.success("Chamado criado com sucesso!");
+				setComplemento("");
+				setCustomerSelected(0);
+			})
+			.catch((error) => {
+				toast.error("Ops erro ao registrar, tente mais tarde.");
+				console.log(error);
+			});
 	}
 
 	function handleChangeSelect(e) {
@@ -20,6 +79,10 @@ export default function New() {
 
 	function handleOptionChange(e) {
 		setStatus(e.target.value);
+	}
+
+	function handleChangeCustomers(e) {
+		setCustomerSelected(e.target.value);
 	}
 
 	return (
@@ -32,13 +95,25 @@ export default function New() {
 				<div className="container">
 					<form className="form-profile" onSubmit={handleRegister}>
 						<label>Cliente</label>
-						<select>
-							<option key={1} value={1}>
-								Sujeito Programador
-							</option>
-						</select>
+						{loadCustomers ? (
+							<input
+								type="text"
+								disabled={true}
+								value="Carregando clientes..."
+							/>
+						) : (
+							<select value={customerSelected} onChange={handleChangeCustomers}>
+								{customers.map((item, index) => {
+									return (
+										<option key={item.id} value={index}>
+											{item.nomeFantasia}
+										</option>
+									);
+								})}
+							</select>
+						)}
 						<label>Assunto</label>
-						<select>
+						<select value={assunto} onChange={handleChangeSelect}>
 							<option value="Suporte">Suporte</option>
 							<option value="Visita Técnica">Visita Técnica</option>
 							<option value="Financeiro">Financeiro</option>
